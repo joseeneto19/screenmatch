e757fed8d879b048f66321dfcf89f80abe4aee5f
joseeneto19 <josemnetodev@gmail.com>
2025-04-20 10:59:21 -0300

    Adicionado modificadores de acesso

--- src/Filme.java
+++ src/Filme.java
@@ -2,9 +2,13 @@ public class Filme {
     String nome;
     int anoDeLancamento;
     boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
     int duracaoEmMinutos;
+
+    int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
 
     void exibeFichaTecnica() {
         System.out.println("Nome do filme: " + nome);
@@ -19,5 +23,7 @@ public class Filme {
     double pegaMedia() {
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
+
+
 
 }
