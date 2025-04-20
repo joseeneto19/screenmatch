3d1adbc6bebe4805f8bc8b7bf91ffefb1b320ebd
joseeneto19 <josemnetodev@gmail.com>
2025-04-20 11:30:36 -0300

    Adicionado herança

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.screenmatch.modelo.Filme;
+import br.com.screenmatch.modelo.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -13,6 +14,14 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+
 
     }
 }
---
+++ src/br/com/screenmatch/modelo/Serie.java
@@ -0,0 +1,40 @@
+package br.com.screenmatch.modelo;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private boolean ativa;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodios;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getMinutosPorEpisodios() {
+        return minutosPorEpisodios;
+    }
+
+    public void setMinutosPorEpisodios(int minutosPorEpisodios) {
+        this.minutosPorEpisodios = minutosPorEpisodios;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+}
